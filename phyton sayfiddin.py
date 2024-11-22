caadae98b6c5637fc3cf15672c12070efba5f97f
Sayfiddin0912 <maxamadovsayfiddin6@gmail.com>
2024-11-22 22:06:51 +0500

    Add files via upload

---
+++ phyton sayfiddin.py
@@ -0,0 +1,24 @@
+import numpy as np 
+                           #MAXAMADOV SAYFIDDIN
+# 1. massiv yaratish
+olma = np.array([1,2,3,4,5])
+nok =np.array([[1,2,3],[4,5,6]])
+
+# 2. matematik yaratish
+qoshish=np.sum(olma)
+ortacha=np.mean(olma)
+kop=np.prod(olma)
+qoshish1 =np.sum(nok)
+ortacha1 =np.mean(nok)
+kop1 =np.prod(nok)
+print("olma masivi ", olma )
+print("nok:\2",nok )
+
+print("massivlar yIgindisi",qoshish)
+print("massivlar ortachasi",ortacha)
+print("massivlar kopaytmasi",kop)
+
+print("massivlar yIgindisi",qoshish1)
+print("massivlar ortacha ",ortacha1)
+print("massivlar kopaytma ",kop1)
+                            #MAXAMADOV SAYFIDDIN
